0c2a9fe6f8d2170f80dde0f5c0151b74cb7d9d24
kb-code-quest <kb-code-quest@outlook.co.nz>
2024-06-11 11:37:34 +1200

    add feature toggle decision logic

--- src/components/earth.tsx
+++ src/components/earth.tsx
@@ -1,3 +1,26 @@
+import { FlagKeys, useFlag } from "../feature-flags/feature-flag-provider";
+
 export default function Earth() {
-  return <div className="">{<h1>Earth</h1>}</div>;
+  const earthFlag = useFlag(FlagKeys.earth);
+
+  const WellingtonWeather = getWellyWeather();
+
+  let selected = false;
+
+  if (earthFlag) {
+    if (WellingtonWeather === "windy") {
+      selected = false;
+    }
+
+    if (WellingtonWeather === "sunny") {
+      selected = true;
+    }
+  }
+
+  return <div className="App">{selected && <h1>Earth</h1>}</div>;
+}
+
+function getWellyWeather() {
+  const i = Math.floor(Math.random() * 2);
+  return ["windy", "sunny"][i];
 }
--- src/components/mercury.tsx
+++ src/components/mercury.tsx
@@ -1,3 +1,19 @@
+import { FlagKeys, useFlag } from "../feature-flags/feature-flag-provider";
+
 export default function Mercury() {
-  return <div className="">{<h1>Mercury</h1>}</div>;
+  const mercuryFlag = useFlag(FlagKeys.mercury);
+
+  let selected = false;
+
+  const mercuryPublicHolidayDate = "2024-6-1";
+
+  if (mercuryFlag === true) {
+    if (new Date() > new Date(mercuryPublicHolidayDate)) {
+      selected = true;
+    } else {
+      selected = false;
+    }
+  }
+
+  return <div className="App">{selected && <h1>Mercury</h1>}</div>;
 }
